decccf61c997956154c4770b0e7e0d596bd4bb25
grace-luning-fu <grace.luning.fu@gmail.com>
2017-08-08 01:16:29 -0400

    first commit

---
+++ src/main/java/com/grace/favoriterecipe/FavoriterecipeApplication.java
@@ -0,0 +1,12 @@
+package com.grace.favoriterecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavoriterecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FavoriterecipeApplication.class, args);
+	}
+}
